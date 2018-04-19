0dd742eb5ed3406285c9b055e6e89d9ccb681511
Kenneth Perry (thothonegan) <thothonegan@gmail.com>
2018-04-19 01:14:02 -0500

    If a null expression is returned, dont try to output it.

--- WexprTool/Private/Application.cpp
+++ WexprTool/Private/Application.cpp
@@ -138,12 +138,17 @@ int main (int argc, char** argv)
 		}
 		else
 		{
-			char* buffer = wexpr_Expression_createStringRepresentation (
-				expr, 0, WexprWriteFlagHumanReadable
-			);
-			
-				s_writeAllOutputTo(results.outputPath, std::string(buffer));
-			free (buffer);
+			if (wexpr_Expression_type(expr) == WexprExpressionTypeNull)
+			{} // right now we cant really output a null expression
+			else
+			{
+				char* buffer = wexpr_Expression_createStringRepresentation (
+					expr, 0, WexprWriteFlagHumanReadable
+				);
+				
+					s_writeAllOutputTo(results.outputPath, std::string(buffer));
+				free (buffer);
+			}
 		}
 
 		wexpr_Expression_destroy (expr);
