5d502f7f058c62da5092dfa0760ef7b48749086c
Kenneth Perry (thothonegan) <thothonegan@gmail.com>
2018-04-19 20:12:40 -0500

    Start of spec, implementation tests

---
+++ ImplementationTests/runner.rb
@@ -0,0 +1,61 @@
+#!/usr/bin/env ruby
+
+tests = []
+
+scriptDir = File.expand_path(File.dirname(__FILE__))
+
+commandToRun = ""
+ARGV.each do|a|
+  commandToRun += (a + " ")
+end
+
+if commandToRun == "" or ! commandToRun.include? "{}"
+	puts ">> Pass the command to run with {} for where to put input files"
+	puts ">> for example: ./runner.rb WexprTool -c validate -i {}"
+	exit 1
+end
+
+Dir["#{scriptDir}/success/*.wexpr"].each do |file|
+	tests << {
+		fileName: file,
+		shouldBeCorrect: true
+	}
+end
+
+Dir["#{scriptDir}/fail/*.wexpr"].each do |file|
+	tests << {
+		fileName: file,
+		shouldBeCorrect: false
+	}
+end
+
+pass=0
+fail=0
+ran=0
+
+tests.each do |testData|
+	fileName = testData[:fileName]
+	shortFileName = fileName.sub "#{scriptDir}/", ""
+	
+	puts "#{ran+1}) #{shortFileName}..."
+	
+	cmd=commandToRun.sub '{}', fileName
+	
+	output = `#{cmd}`
+	
+	if $?.success? == testData[:shouldBeCorrect]
+		pass += 1
+	else
+		puts ""
+		puts "!!! FAIL: #{fileName} : got #{$?.success?} but expected #{testData[:shouldBeCorrect]}"
+		puts ""
+		fail += 1
+	end
+	
+	ran += 1
+end
+
+puts "---"
+puts "Pass: #{pass} (#{(pass*1.0/(ran)*100).to_i}%)"
+puts "Fail: #{fail} (#{(fail*1.0/(ran)*100).to_i}%)"
+puts "Total: #{ran}"
