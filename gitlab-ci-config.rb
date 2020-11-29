25fabf36baf680581b79340e7310ae2b9d9cab75
Kenneth Perry (thothonegan) <thothonegan@hackerguild.com>
2020-11-28 19:49:22 -0500

    Only CI on linux due to OSX currently being broken

--- gitlab-ci-config.rb
+++ gitlab-ci-config.rb
@@ -1,12 +1,10 @@
 HGUILD_PROJECT="libWexpr"
 HGUILD_PROFILES={
 	"Linux-X86_64@Clang@@" => ["Debug", "Release"],
-	"macOS-X86_64@Clang@@" => ["Debug", "Release"]
+	#"macOS-X86_64@Clang@@" => ["Debug", "Release"]
 }
 
 GITLABCI_SCHEDULER="Support/gitlabScheduler.rb"
-
-# We use macOS due to docset generation requiring docsetutil
-GITLABCI_SCHEDULER_PROFILE="macOS-X86_64@Clang@@"
+GITLABCI_SCHEDULER_PROFILE="Linux-X86_64@Clang@@"
 GITLABCI_SCHEDULER_BUILDTYPE="Debug"
 
