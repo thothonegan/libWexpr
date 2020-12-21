11aab89b1d9f80e557fcd552ea925533a6eea79a
Kenneth Perry (thothonegan) <thothonegan@hackerguild.com>
2020-12-21 14:36:48 -0500

    Add ARM64 CI support

--- gitlab-ci-config.rb
+++ gitlab-ci-config.rb
@@ -1,7 +1,8 @@
 HGUILD_PROJECT="libWexpr"
 HGUILD_PROFILES={
 	"Linux-X86_64@Clang@@" => ["Debug", "Release"],
-	#"macOS-X86_64@Clang@@" => ["Debug", "Release"]
+	"macOS-ARM64@Clang@@" => ["Debug", "RelWithDebInfo"],
+	"macOS-ARM64@Clang@macOS-X86_64@" => ["Debug", "RelWithDebInfo"]
 }
 
 GITLABCI_SCHEDULER="Support/gitlabScheduler.rb"
