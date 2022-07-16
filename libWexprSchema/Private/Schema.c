56f41abedb3fb92745fb38e603fa89b427f09d28
Kenneth Perry (thothonegan) <thothonegan@hackerguild.com>
2022-07-16 06:12:40 -0400

    More schema work

--- libWexprSchema/Private/Schema.c
+++ libWexprSchema/Private/Schema.c
@@ -8,20 +8,60 @@
 
 #include <libWexprSchema/Schema.h>
 
+#include <libWexpr/libWexpr.h>
+
+#include <stdio.h>
 #include <stdlib.h>
+
+#include "../../libWexpr/Private/ThirdParty/sglib/sglib.h"
 
 struct WexprSchemaSchema
 {
+	//
+	/// \brief Callbacks used for this schema
+	//
 	WexprSchemaSchema_Callbacks m_callbacks;
+	
+	//
+	/// \brief The ID of the schema
+	//
+	char* m_id;
+	
+	//
+	/// \brief The schema the schema is following
+	/// Can be null if its the root schema.
+	//
+	WexprSchemaSchema* m_schema;
+	
+	//
+	/// \brief The title of the schema
+	//
+	char* m_title;
+	
+	//
+	/// \brief The description of the schema
+	//
+	char* m_description;
+	
+	// referenceSchemas
+	
+	// types
+	
+	// rootType
+	
 };
 
 static void* s_defaultAlloc (void* allocatorUserData, size_t size)
 {
+	(void)allocatorUserData;
+	
 	return malloc(size);
 }
 
 static void s_defaultDealloc (void* allocatorUserData, void* ptr)
 {
+	(void)allocatorUserData;
+	
 	free(ptr);
 }
 
@@ -31,17 +71,74 @@ static const char* s_defaultPathForSchemaID (void* userData, const char* schemaI
 	return LIBWEXPR_NULLPTR;
 }
 
+//
+/// \brief Create a string containing the contents of the path
+/// \note Allocated with callbacks
+//
+static char* s_createStringFromLocation (const char* path, WexprSchemaSchema_Callbacks* callbacks, WexprSchemaError** error)
+{
+	if (strstr(path, "http") == path)
+	{
+		// TODO: curl
+		if (error) {
+			*error = wexprSchema_Error_create(
+				WexprSchemaErrorInternal,
+				"/",
+				"Unable to load schema from http/https"
+			);
+		}
+
+		return LIBWEXPR_NULLPTR;
+	}
+	else
+	{
+		FILE* f = fopen(path, "rb");
+		fseek(f, 0, SEEK_END);
+		long int end = ftell(f);
+		fseek(f, 0, SEEK_SET);
+		
+		char* buf = callbacks->alloc(callbacks->allocatorUserData, end);
+		if (buf == LIBWEXPR_NULLPTR)
+		{
+			if (error) {
+				*error = wexprSchema_Error_create(
+					WexprSchemaErrorInternal,
+					"/",
+					"Unable to allocate memory"
+				);
+			}
+			return LIBWEXPR_NULLPTR;
+		}
+		fread(buf, end, 1, f);
+		fclose(f);
+		
+		return buf;
+	}
+}
+
 static bool s_loadFromSchemaID (WexprSchemaSchema* self, const char* schemaID, WexprSchemaError** error)
 {
-	if (error) {
-		*error = wexprSchema_Error_create(
-			WexprSchemaErrorInternal,
-			"/",
-			"Unable to load schema"
-		);
-	}
-
-	return false;
+	WexprSchemaSchema_Callbacks* callbacks = &(self->m_callbacks);
+	const char* path = callbacks->pathForSchemaID(self->m_callbacks.pathForSchemaIDUserData, schemaID);
+	if (path == LIBWEXPR_NULLPTR)
+		path = schemaID;
+	
+	char* str = s_createStringFromLocation(path, callbacks, error);
+	if (str == LIBWEXPR_NULLPTR)
+		return false;
+	
+	WexprError err = WEXPR_ERROR_INIT();
+	
+	WexprExpression* wexpr = wexpr_Expression_createFromLengthString(
+		str, strlen(str), WexprParseFlagNone, &err
+	);
+	
+	
+	
+	WEXPR_ERROR_FREE(err);
+	callbacks->dealloc(callbacks->allocatorUserData, str);
+	
+	return true;
 }
 
 // --- public Construction/Destructino
@@ -82,6 +179,22 @@ WexprSchemaSchema* wexprSchema_Schema_createFromSchemaID(const char* schemaID, W
 
 void wexprSchema_Schema_destroy(WexprSchemaSchema* self)
 {
+	WexprSchemaSchema_Callbacks* callbacks = &(self->m_callbacks);
+	
+	if (self->m_id)
+		callbacks->dealloc(callbacks->allocatorUserData, self->m_id);
+	
+	if (self->m_schema)
+	{
+		wexprSchema_Schema_destroy(self->m_schema);
+	}
+	
+	if (self->m_title)
+		callbacks->dealloc(callbacks->allocatorUserData, self->m_title);
+	
+	if (self->m_description)
+		callbacks->dealloc(callbacks->allocatorUserData, self->m_description);
+	
 	self->m_callbacks.dealloc(self->m_callbacks.allocatorUserData, self);
 }
 
@@ -89,5 +202,15 @@ void wexprSchema_Schema_destroy(WexprSchemaSchema* self)
 
 bool wexprSchema_Schema_validateExpression(WexprExpression* expression, WexprSchemaError** error)
 {
+	if (error) {
+		*error = wexprSchema_Error_create(
+			WexprSchemaErrorInternal,
+			"/",
+			"Unable to validate"
+		);
+	
+		return false;
+	}
+	
 	return false;
 }
